a6a254e627fa7cfd7d02337c7b863ada09577523
Farah Abusalim <farahabusaleem9@gmail.com>
2025-05-06 19:51:32 +0300

    Cleanup: use only /api/users/ for user_management endpoints

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,24 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView,)
-from rest_framework_simplejwt.views import TokenVerifyView
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #JWT Authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
-# myproject/urls.py
-from django.contrib import admin
-from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user_management/', include('user_management.urls')), 
-    path('api/users/', include('user_management.urls')), 
-    path('', include('user_management.urls')),
-    path('ads/', include('advertisements.urls')),
+    #User management(register/, login/, change-password/)
+    path('api/users/', include('user_management.urls')),  # ✅ استخدم فقط هذا
+
+    #Advertisements
+    path('api/ads/', include('advertisements.urls')),
+
+    # Q&A section
     path('api/qa/', include('qa.urls')),
 ]
